72d66016c70b34923d8d5e63560825d5ac3b9cb9
Bakdaulet <bakdaulettepbaj5@gmail.com>
2026-05-17 23:14:24 +0500

    feat(user-service): initialize go module and basic gRPC server setup

---
+++ user-service/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	fmt.Println("User Service is running on port :50051...")
+	
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
